ce024618105549225b7f5bcf9d248fcf7c155535
PhMoraiis <liperapltda@gmail.com>
2023-04-17 20:28:58 -0300

    making the exchange of technologies for scaling

--- db.js
+++
@@ -1,9 +0,0 @@
-import mysql from 'mysql';
-
-export const db = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: '7675',
-    database: 'crudwine'
-});
-
--- routes/vinicolas.js
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { getVinicolas } from '../controllers/vinicola.js';
-
-const router = express.Router();
-
-router.get('/', getVinicolas);
-
-export default router;
