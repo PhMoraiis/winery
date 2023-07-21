1ce10570d9e6ef948eb81f72a846f8474ac2350c
PhMoraiis <liperapltda@gmail.com>
2023-07-21 10:11:13 -0300

    create migration populate

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:latest
+FROM node:18-alpine
 
 RUN npm install -g npm@latest postgresql-client
 
--- app/data-source.ts
+++ app/data-source.ts
@@ -1,14 +1,15 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
+import "dotenv/config"
 
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    host: "localhost",
-    port: 5432,
-    username: "phmorais",
-    password: "winerydatapass",
-    database: "winery_database",
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT ? Number(process.env.DB_PORT) : 5432,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS ?? "winerydatapass",
+    database: process.env.DB_NAME,
     entities: [`${__dirname}/**/entities/*.{ts,js}`],
 	migrations: [`${__dirname}/**/migrations/*.{ts,js}`],
 })
